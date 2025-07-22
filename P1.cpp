726c5d8aa5d471d0c0a6e401a975114be2ecce6e
Varnika-06 <varnikastuff234@gmail.com>
2025-07-22 14:44:16 +0530

    Create P1.cpp

---
+++ P1.cpp
@@ -0,0 +1,25 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp-6
+//Program 1
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int i;
+    for(i=0;i<5;i++)
+    {
+        cout<<"SIT \n";
+    }
+    return 0;
+}
+/*
+PS C:\Users\DELL\Desktop\C++> g++ L5.cpp -o L5
+PS C:\Users\DELL\Desktop\C++> .\L5.exe        
+SIT 
+SIT
+SIT
+SIT
+SIT
+  */
