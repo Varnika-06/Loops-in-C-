1064a2f9dc318a2b54861c9ce7e02f431a98b47c
Varnika-06 <varnikastuff234@gmail.com>
2025-07-22 14:47:18 +0530

    Create P2.cpp

---
+++ P2.cpp
@@ -0,0 +1,25 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp-6
+//Program 2
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int i;
+for(i=0;i<=10;i++)    {
+    if(i%2==0)
+        cout<<i<<"\n";
+    }
+
+    return 0;
+}
+/*
+PS C:\Users\DELL\Desktop\C++> g++ P2.cpp -o P2
+PS C:\Users\DELL\Desktop\C++> .\P2.exe        
+0
+2
+4
+6
+8
+10*/
