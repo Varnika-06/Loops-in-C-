c1d677414dcf1d05920c65ea65a0f1e6965fa856
Varnika-06 <varnikastuff234@gmail.com>
2025-07-22 14:52:11 +0530

    Create P3.cpp

---
+++ P3.cpp
@@ -0,0 +1,38 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp-6
+//Program 3
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<20;i++)
+    {
+        cout<<i+1<<"\n";
+    }
+    return 0;
+}
+/*
+PS C:\Users\DELL\Desktop\C++> g++ L5.cpp -o L5
+PS C:\Users\DELL\Desktop\C++> .\L5.exe        
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+  */
