858d0cbdccc7f3db53f733ea69dfcb8c05a82494
Varnika-06 <varnikastuff234@gmail.com>
2025-07-29 01:37:49 +0530

    Create P10.cpp

---
+++ P10.cpp
@@ -0,0 +1,31 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp-6
+//Program 10
+#include <iostream>
+using namespace std;
+int main() {
+    int n, i, j;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
+/*
+PS C:\Users\DELL\Desktop\C++> g++ l6.cpp -o l6
+PS C:\Users\DELL\Desktop\C++> .\l6.exe        
+Enter the number of rows: 5
+*****
+****
+***
+**
+*
+  */
