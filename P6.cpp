2a0df765b1df4f805d772d480d53193b3fbfbe4d
Varnika-06 <varnikastuff234@gmail.com>
2025-07-29 01:28:14 +0530

    Create P6.cpp

---
+++ P6.cpp
@@ -0,0 +1,28 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp-6
+//Program 1
+#include <iostream>
+using namespace std;
+int main() {
+     int i,j;
+     for(i=0;i<5;i++)
+     {
+        for(j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+     }
+    return 0;
+}
+
+/*
+PS C:\Users\DELL\Desktop\C++> g++ P6.cpp -o P6
+PS C:\Users\DELL\Desktop\C++> .\P6.exe        
+*
+**
+***
+****
+*****
+  */
